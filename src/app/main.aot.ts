31894ab10adaf52d312dab7b22704ab1caadfd56
villalbaweb <villalbaweb@gmail.com>
2018-05-06 12:42:39 -0500

    Adding changes to be able and compile the project for prod environment

--- src/app/main.aot.ts
+++ src/app/main.aot.ts
@@ -1,4 +1,4 @@
-import '../polifylls';
+import '../polyfills';
 
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode }  from '@angular/core';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,35 +2,37 @@ var path = require('path');
 
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var commonCOnfig = require('./webpack.config.common');
+var commonConfig = require('./webpack.config.common');
+var ngw = require('@ngtools/webpack');
 
-module.exports = webpackMerge(commonCOnfig, {
-    entry: './src/app/main/aot/ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: '[hash].js',
-        chunkFilename: '[id].[hash].chunk.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    { 
-                        loader: 'awesome-typescript-loader'
-                    },
-                    {
-                        loader: 'angular2-template-loader'
-                    },
-                    {
-                        loader: 'angular-router-loader?aot=true'
-                    }
-                ]
-            }
+module.exports = webpackMerge(commonConfig, {
+  entry: './src/app/main.aot.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+        loader: '@ngtools/webpack'
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {loader: 'awesome-typescript-loader'},
+          {loader: 'angular2-template-loader'},
+          // {loader: 'angular-router-loader?aot=true'}
         ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]    
+      }
+    ]
+  },
+  plugins: [
+    new ngw.AngularCompilerPlugin({
+      tsConfigPath: './tsconfig.aot.json',
+      entryModule: './src/app/app.module#AppModule'
+    })
+  ]
 });
